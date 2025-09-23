456e67ea38bb12c04ff4707229b9f71e676a7699
Ricardo André <91620783+rickzerahh@users.noreply.github.com>
2025-09-22 22:04:47 -0300

    seta surface

--- site-de-filmes/componentes/MovieRow.js
+++ site-de-filmes/componentes/MovieRow.js
@@ -1,19 +1,40 @@
 "use client";
+import { useRef } from "react";
 import MovieCard from "./MovieCard";
 import "../styles/MovieRow.css";
 
 export default function MovieRow({ movies }) {
+  const rowRef = useRef(null);
 
   const uniqueMovies = movies?.filter(
     (movie, index, self) =>
       index === self.findIndex((m) => m.id === movie.id)
   );
 
+  const scroll = (direction) => {
+    if (rowRef.current) {
+      const { scrollLeft, clientWidth } = rowRef.current;
+      const scrollAmount = direction === "left" ? -clientWidth : clientWidth;
+      rowRef.current.scrollTo({
+        left: scrollLeft + scrollAmount,
+        behavior: "smooth",
+      });
+    }
+  };
+
   return (
-    <div className="row">
-      {uniqueMovies.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} />
-      ))}
+    <div className="movieRowWrapper">
+      <button className="scrollBtn left" onClick={() => scroll("left")}>
+        ◀
+      </button>
+      <div className="row" ref={rowRef}>
+        {uniqueMovies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+      <button className="scrollBtn right" onClick={() => scroll("right")}>
+        ▶
+      </button>
     </div>
   );
 }
